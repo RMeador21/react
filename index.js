77d1d4ed147783cf785266f9ae80667b945361fe
Eric <Richard.e.meador@gmail.com>
2023-02-04 17:28:05 -0500

    Changed the JS file so it would work

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 ReactDOM.render(<p>Hi, my name is Eric!</p>, document.getElementById("root"))
+const hello = (
+    <h1>Hello Everyone, my name is Eric</h1>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(hello)
